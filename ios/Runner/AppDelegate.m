f94d734374f3b2e79c41959511085a4861fdceea
MorganReilly <morgan.reilly27@gmail.com>
2019-07-25 11:48:47 +0100

    Hybrid map implemented, playing around with UI. Adding list of Google Offices

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,13 +1,13 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
+#import "GoogleMaps/GoogleMaps.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [GMSServices provideAPIKey:@"AIzaSyAfKAjPggknQQ8leYOtmNY0x_Zk9skRoRM"];
   [GeneratedPluginRegistrant registerWithRegistry:self];
-  // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
-
 @end
